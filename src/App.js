4480e1a9f5e04fb045e591bfd1703574d4554927
김준호 <enddl0121@gmail.com>
2020-07-29 15:57:48 +0900

    함수형 다하고 서버 만지기

--- src/App.js
+++ src/App.js
@@ -1,97 +1,133 @@
-import React, {useState, useEffect} from "react";
-import events from './events';
-import { Calendar, momentLocalizer, Views, DateLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import 'moment/locale/ko'
-import _ from 'lodash'
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+import React, { useState, useEffect } from "react";
+import events from "./events";
+import _ from "lodash";
+import {
+  Calendar,
+  momentLocalizer,
+  Views,
+} from "react-big-calendar";
+import moment_timezone from "moment-timezone";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.scss";
 
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss'
+moment_timezone.tz.setDefault("Asia/Seoul");
 
-moment.locale("ko")
-
-
-const DragAndDropCalendar = withDragAndDrop(Calendar)
+const DragAndDropCalendar = withDragAndDrop(Calendar);
 
 // -------------------------------------------------
 function Event({ event }) {
   return (
     <span>
-      <strong>{event.title}</strong>
-      <p>{event.desc && ' : ' + event.desc}</p>  
+      <strong style={{color:"Coral"}}>{event.title}</strong>
     </span>
-  )           //마우스 오버하기
+  )
 }
 
 //Agenda에서 이벤트 주는거
 function EventAgenda({ event }) {
   return (
     <span>
-      <em style={{ color: 'magenta' }}>{event.title}</em>
+      <em style={{ color: "magenta" }}>{event.title}</em>
       <p>{event.desc}</p>
     </span>
-  )
+  );
 }
 
-function App(props) {
+//allday 이거 지웠고, 
 
-  const [Events, setEvents] = useState(events)
-  const [dayLayoutAlgorithm, setdayLayoutAlgorithm] = useState('no-overlap')
-  const [displayDragItemInCell, setdisplayDragItemInCell] = useState(true)
+function App() {
+  const [Events, setEvents] = useState(_.cloneDeep(events));
+  // const [Events, setEvents] = useState(events);
+  const [dayLayoutAlgorithm, setdayLayoutAlgorithm] = useState("no-overlap");
 
+  function handleSelect({ start, end }){
+    const title = window.prompt("일정을 추가하세요");
+    const desc = window.prompt("내용을 추가하세요");
+    if (title) 
+      setEvents(
+        (events) => [
+          ...events, 
+          { 
+            title,
+            start,
+            end,
+            desc,
+          }
+      ]
+    );
+  };
+
+//? : 이해하기
+  function moveEvent({ event, start, end}){
+    const nextEvents = Events.map((existingEvent) => {
+      console.log(event.title)
+      return existingEvent.title === event.title
+        ? { ...existingEvent, start, end }
+        : existingEvent;
+    });
+    setEvents(nextEvents)
+  };
+
+
+  function resizeEvent({ event, start, end}){
+    const nextEvents = Events.map((existingEvent) => {
+      return existingEvent.title === event.title
+        ? { ...existingEvent, start, end }
+        : existingEvent;
+    });
+    setEvents(nextEvents)
+  };
+
+
+  function onSelectEvent(pEvent) {
+    const r = window.confirm("일정을 취소합니다.");
+    if (r === true) {
+      setEvents((preEvents) => {
+        const events = [...preEvents];
+        const idx = events.indexOf(pEvent);
+        events.splice(idx, 1);
+        return events;
+      });
+    }
+  }
 
   
-  const handleSelect = ({ start, end }) => {
-    const title = window.prompt('일정을 추가하세요')
-    const desc = window.prompt("내용을 추가하세요")
-//     if (title)
-//     setEvents([...Events, {start,end,title,desc}])
-// }
-
-    if (title)
-      setEvents(events => [...events,{start,end,title,desc}])
-    }
-
-
-  const localizer = momentLocalizer(moment)
+  const localizer = momentLocalizer(moment_timezone);
   return (
     <>
       <h1>일정관리</h1>
-        <DragAndDropCalendar
-          
-          style={{ height: 800 ,width: '100%' }}
-          popup={true}                            //popup 만들어주는거 넘어갓을 때
-          selectable={true}                              //**선택할수 있게 만들어줌
-          localizer={localizer}                                     //moment 모듈을 이용한 로컬화
-          events={Events}                               //이벤트 나오게 하는거
-          defaultView={Views.MONTH}                                  //디폴트 뷰
-          scrollToTime={new Date()}                               //**스크롤 시작 위치를 정해줌(안해줘도 될듯)
-          defaultDate={moment().toDate()}                           //디폴트 날짜
-          onSelectSlot={handleSelect}                          //**날짜 선택시 콜백이 발생한다 -> 위에서 만들어준 handleSelect가 실행
-          dayLayoutAlgorithm={dayLayoutAlgorithm}        //레이아웃 배열의 알고리즘
-          
-        //   onEventDrop={this.moveEvent}
-        //   onEventResize={this.resizeEvent}
-        // // onDragStart={console.log}                                //콘솔로그 찍히는거 드래그 시작할 떄
-        // //   dragFromOutsideItem={
-        // //   this.state.displayDragItemInCell ? this.dragFromOutsideItem : null
-        // // }
-        // onDropFromOutside={this.onDropFromOutside}
-        // handleDragStart={this.handleDragStart}             
-        // tooltipAccessor={this.state.events.start}                 //작동안됌
-        // onDoubleClickEvent = { event => this.onSelectEvent(event) } //**날자 말고 일정 더블클릭으로 업데이트 해보기      
+      <DragAndDropCalendar
+        style={{ height: 800, width: "100%" }}
+        popup={true}       //+ _x_ more"링크를 클릭하면 잘린 이벤트를 오버레이에 표시합니다.
+        selectable={true}   //필수 ** 날짜와 범위를 선택할수 있게 만들어줌
+        localizer={localizer} //moment 모듈을 이용한 로컬화
+        events={Events} //이벤트 나오게 하는거
+        startAccessor="start"
+        endAccessor="end"
+        defaultView={Views.MONTH} //디폴트 뷰
+        scrollToTime={new Date()} //**스크롤 시작 위치를 정해줌(안해줘도 될듯)
+        defaultDate={moment_timezone().toDate()} //디폴트 날짜
+        onSelectSlot={handleSelect} //**날짜 선택시 콜백이 발생한다 -> 위에서 만들어준 handleSelect가 실행
+        dayLayoutAlgorithm={dayLayoutAlgorithm} //레이아웃 배열의 알고리즘
+        onDragStart={console.log}              //콘솔로그 찍히는거 드래그 시작할 떄  
 
+        
+        onEventDrop={moveEvent}
+        onEventResize={resizeEvent}
+        onDoubleClickEvent={(event) => onSelectEvent(event)}
+        
         components={{
-        events: Event,   //여기서 호버줘야함
-        agenda: {
-          event: EventAgenda,
-        },
-      }}
-        />
+          event: Event, //여기서 호버줘야함
+          agenda: {
+            event: EventAgenda,
+          },
+          
+        }}
+      />
     </>
   );
-};
+}
 
 export default App;
--- src/events.js
+++ src/events.js
@@ -1,32 +1,37 @@
 export default [
   {
     'title': 'All Day Event very long title',
-    'allDay': true,
+    // 'allDay': true,
     'start': new Date(2020, 3, 0),
     'end': new Date(2020, 3, 1),
+    'desc': ''
   },
   {
     'title': 'Long Event',
     'start': new Date(2020, 7, 7),
-    'end': new Date(2020, 7, 10)
+    'end': new Date(2020, 7, 10),
+    'desc': ''
   },
 
   {
     'title': 'DTS STARTS',
     'start': new Date(2020, 2, 13, 0, 0, 0),
-    'end': new Date(2020, 2, 20, 0, 0, 0)
+    'end': new Date(2020, 2, 20, 0, 0, 0),
+    'desc': ''
   },
 
   {
     'title': 'DTS ENDS',
     'start': new Date(2020, 10, 6, 0, 0, 0),
-    'end': new Date(2020, 10, 13, 0, 0, 0)
+    'end': new Date(2020, 10, 13, 0, 0, 0),
+    'desc': ''
   },
 
   {
     'title': 'Some Event',
     'start': new Date(2020, 8, 9, 0, 0, 0),
-    'end': new Date(2020, 8, 10, 0, 0, 0)
+    'end': new Date(2020, 8, 10, 0, 0, 0),
+    'desc': ''
   },
   {
     'title': 'Conference',
